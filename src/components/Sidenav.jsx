fd8062e1b81fbe758067847aaca370116abbc10c
Nitesh Prajapat <niteshkmrprjapati298@.com>
2024-09-25 14:46:23 +0530

    first commit

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Sidenav = () => {
+  return <>
+    <Box as='h2'>
+      Nitesh 
+    </Box>;
+  </>
+}
+
+export default Sidenav
